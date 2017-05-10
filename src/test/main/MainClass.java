4a6e76282980a89c98672ca4fbeb5966db47c78c
donggyu <hdg4618@naver.com>
2017-05-10 12:11:12 +0900

    My, World 출력함

--- src/test/main/MainClass.java
+++ src/test/main/MainClass.java
@@ -4,5 +4,7 @@ public class MainClass {
 	public static void main(String[] args){
 		//콘솔창에 Hello, world 문자열 출력하기
 		System.out.println("Hello,world!");
+		//콘솔창에 My, world 문자열 출력하기
+		System.out.println("My,World!");
 	}
 }
